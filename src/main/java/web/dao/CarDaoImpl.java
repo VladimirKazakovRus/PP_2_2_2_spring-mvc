820006213208472ca43b76ccf02b1440bc0d7b03
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-08 00:58:11 +0500

    add controller and html + dependencies + support utf8

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,4 +1,29 @@
 package web.dao;
 
-public class CarDaoImpl {
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarDaoImpl implements CarDao{
+    private static final List<Car> cars = new ArrayList<>();
+    static {
+        cars.add(new Car("модель1","цвет1",2001,1000000));
+        cars.add(new Car("модель2","цвет2",2002,1100000));
+        cars.add(new Car("модель3","цвет3",2003,2000000));
+        cars.add(new Car("модель4","цвет4",2004,3000000.50));
+        cars.add(new Car("модель5","цвет5",2005,900000.00));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        if (count == 0) {
+            return cars;
+        }
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,9 +1,11 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import web.model.Car;
 
 import java.util.List;
 
 public interface CarService {
-    List<Car> GetCars(int count);
+    @Autowired
+    List<Car> getCars(int count);
 }
